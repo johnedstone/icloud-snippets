e0c3a4bb386ce76386afb1abb41418261b95bc43
johnedstone.net <johnedstone@gmail.com>
2021-10-13 14:20:35 -0400

    working on download script

--- album_download_03.py
+++ album_download_03.py
@@ -4,12 +4,30 @@ import sys
 from datetime import datetime
 from pprint import pprint
 
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
 logging.basicConfig(
-    level=logging.INFO, format="[%(asctime)s] %(message)s", datefmt="%d/%m/%y %H:%M:%S"
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s", datefmt="%d/%m/%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler('logs/download.logs'),
+        logging.StreamHandler()
+    ]
 )
 
+def album_download(api=None, album_list=[], exclude_list=[]):
+    try:
+        for d in album_list:
+            if not os.path.exists(d):
+                os.mkdir(d)
+        logging.info('album_list: {}'.format(album_list))
+    except Exception as e:
+        pass
+    finally:
+        logging.info('Finished downloading')
+
 if __name__ == '__main__':
-    logging.info('boo')
+    logging.info('Starting')
 
 # vim: ai et ts=4 sw=4 sts=4 nu
-
